6324c43aa64d8d14053674fb9225c12324580c9c
nickchell <chelelgon@gmail.com>
2025-05-24 16:52:49 -0600

    Initial commit

--- aurastyle4-main/src/pages/dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const Dashboard: React.FC = () => {
-  // Simple redirect to the wardrobe page
-  return <Navigate to="/dashboard/wardrobe" replace />;
-};
-
-export default Dashboard;
